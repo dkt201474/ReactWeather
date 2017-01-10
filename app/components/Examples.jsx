99da2d7a26f75472837eede9b4cd11cb2aa8f5ac
dkt201474 <dkt201474pro@gmail.com>
2017-01-10 08:20:11 -0500

    Stylize example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,26 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
-        <h3>Example component</h3>
+        <div>
+            <h1>Example</h1>
+            <p>Here are some example of location to try out : </p>
+
+            <ol>
+                <li>
+                    <Link to='/?location=Ottawa'>Ottawa, ON</Link>
+                </li>
+
+                <li>
+                    <Link to='/?location=Toronto'>Toronto, ON</Link>
+                </li>
+
+                <li>
+                    <Link to='/?location=Parakou'>Parakou, BENIN</Link>
+                </li>
+            </ol>
+        </div>
     );
 };
 module.exports = Examples;
